6b40253527870d8a22a11a569514cfc4818fe5bc
Dhanu37400 <dhanarajmalage370@gmail>
2026-06-20 15:11:29 +0530

    updated database

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-   images: {
+  images: {
     unoptimized: true,
+  },
+  turbopack: {
+    root: __dirname,
   },
 };
 
